afe70b0c398f71e6e73a562e0e082c966a681ef9
Thomas Riley <thomas.riley@autotrader.co.uk>
2016-01-06 09:13:20 +0000

    Chef Supermarket release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email ''
 license          'Apache 2.0'
 description      'Installs/Configures logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
 
 issues_url       'https://github.com/thomasriley/logstash_simple/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/thomasriley/logstash_simple' if respond_to?(:source_url)
